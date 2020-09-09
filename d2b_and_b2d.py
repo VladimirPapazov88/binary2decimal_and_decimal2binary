15ba19c1a0ea6b523c5c9b538a20369bcf0b6587
VladimirPapazov88 <71008536+VladimirPapazov88@users.noreply.github.com>
2020-09-09 17:12:05 +0300

    Create d2b_and_b2d.py

---
+++ d2b_and_b2d.py
@@ -0,0 +1,18 @@
+def encode(k):
+        if k // 2 != 0:
+                dvoika(k // 2)
+        csl = k % 2
+        print(csl, sep="", end="")
+
+def decode(k): 
+        leng = len(str(k))
+        kstr = ''.join(reversed(str(k)))
+        answ = 0 
+        for x in range(0, leng):
+                if (kstr!="0"):
+                        answ = (int(kstr[x])*(2**x))+answ
+        print(answ)
+
+# write encode to convert decimal number at binary number
+# write decode to conver binary number at decimal number
+# syntax encode(<your number>) decode(<your number>)
